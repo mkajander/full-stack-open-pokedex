435a017d0a4e9a88c2fafb9ce9ae9d79a7654eac
Mikael Kajander <mb.kajander@gmail.com>
2022-07-10 00:39:35 +0300

    woops + needed to add timeout

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 })))
 
 const App = () => {
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
+  // const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
+  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=20', mapResults)
   if (isLoading) {
     return <LoadingSpinner />
   }
